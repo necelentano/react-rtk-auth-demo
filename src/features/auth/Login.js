92aa6e038c32a101b3d6bb31de54c94e9e81e4d5
Necelentano <necelentano@gmail.com>
2022-08-25 23:14:20 +0300

    Bug fix

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -34,56 +34,63 @@ const Login = () => {
       setPassword("");
       navigate("/welcome");
     } catch (error) {
-        if(!error?.originalStatus) {
-            setErrorMessage('No Server Response')
-        } else if (error.originalStatus === 400) {
-            setErrorMessage('Missing Username or Password')
-        } else if (error.originalStatus === 401) {
-            setErrorMessage('Unauthorized')
-        } else {
-            setErrorMessage(' Login Failed')
-        }
-        errorRef.current.focus()
+      if (!error?.originalStatus) {
+        setErrorMessage("No Server Response");
+      } else if (error.originalStatus === 400) {
+        setErrorMessage("Missing Username or Password");
+      } else if (error.originalStatus === 401) {
+        setErrorMessage("Unauthorized");
+      } else {
+        setErrorMessage("Login Failed");
+      }
+      errorRef.current.focus();
+    }
   };
 
-  const handleUserInput = (e) => setUser(e.target.value) 
+  const handleUserInput = (e) => setUser(e.target.value);
 
-  const handlePasswordInput = (e) => setPassword(e.target.value) 
+  const handlePasswordInput = (e) => setPassword(e.target.value);
 
-
-  const content = isLoading ? <h1>Loading...</h1> : (
+  const content = isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
     <section className="login">
-        <p ref={errorRef} className={errorMessage ? "errmsg" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+      <p
+        ref={errorRef}
+        className={errorMessage ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errorMessage}
+      </p>
 
-        <h1>Employee Login</h1>
+      <h1>Employee Login</h1>
 
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                ref={userRef}
-                value={user}
-                onChange={handleUserInput}
-                autoComplete="off"
-                required
-            />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          value={user}
+          onChange={handleUserInput}
+          autoComplete="off"
+          required
+        />
 
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                id="password"
-                onChange={handlePasswordInput}
-                value={password}
-                required
-            />
-            <button>Sign In</button>
-        </form>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handlePasswordInput}
+          value={password}
+          required
+        />
+        <button>Sign In</button>
+      </form>
     </section>
-)
+  );
 
-  return content
-  
+  return content;
 };
 
 export default Login;
